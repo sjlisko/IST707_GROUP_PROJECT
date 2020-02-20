aa6fe0047ea2e986108045d8a903b7174c7f2842
holden-herrell <58185356+holden-herrell@users.noreply.github.com>
2020-02-20 18:44:07 -0500

    Add files via upload

---
+++ Code/IST707_FinalProject.R
@@ -0,0 +1,36 @@
+###IST707_Final_Project---2019 World Happiness Report
+###ONL-Winter 2020
+###Shawn Anderson
+###Holden Herrell
+###Samantha Brennen-Lisko
+###Jogesh Pugazhendhi
+###Matthew Sutherland
+
+#Load Required Libraries
+
+library(useful)
+library(tm)
+library(stringr)
+library(wordcloud)
+library(slam)
+library(quanteda)
+library(SnowballC)
+library(arules)
+library(proxy)
+library(cluster)
+library(stringi)
+library(proxy)
+library(Matrix)
+library(tidytext) 
+library(plyr) 
+library(ggplot2)
+library(factoextra) 
+library(mclust)
+library(corpus)
+library(rpart)
+library(rpart.plot)
+library(rattle)
+
+#Import WHR dataset
+WHRurl<-"https://raw.githubusercontent.com/holden-herrell/IST707_GROUP_PROJECT/master/Data%20Sources/WHR2019.csv"
+WHRraw<-read.csv(WHRurl)
